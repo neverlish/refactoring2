a1c218453f177e8ef535342247879a019808361b
Jinho Hyeon <neverlish@gmail.com>
2020-05-19 20:13:28 +0900

    06 - 03 - shipping 분리

--- 06/03.js
+++ 06/03.js
@@ -1,8 +1,8 @@
 // 06 - 03 변수 추출하기
 
 function price(order) {
-  // 가격(price) = 기본 가격 - 수량 할인 + 배송비
   const basePrice = order.quantity * order.itemPrice;
   const quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
-  return basePrice - quantityDiscount + Math.min(basePrice * 0.1, 100);
+  const shipping = Math.min(basePrice * 0.1, 100);
+  return basePrice - quantityDiscount + shipping;
 }
