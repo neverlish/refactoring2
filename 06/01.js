e794d8477e8b3f697b846d30d6dc9be8955ca767
Jinho Hyeon <neverlish@gmail.com>
2020-05-19 19:56:05 +0900

    06 - 01 - printDetails 함수 분리

--- 06/01.js
+++ 06/01.js
@@ -14,9 +14,13 @@ function printOwing(invoice) {
   const today = Clock.today;
   invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
 
-  console.log(`고객명: ${invoice.customer}`);
-  console.log(`채무액: ${outstanding}`);
-  console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+  printDetails();
+
+  function printDetails() {
+    console.log(`고객명: ${invoice.customer}`);
+    console.log(`채무액: ${outstanding}`);
+    console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+  }
 }
 
 function printBanner() {
