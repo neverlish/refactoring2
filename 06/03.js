76c9a52dee0a2d41f6f1217f5f16b5495d6a0af9
Jinho Hyeon <neverlish@gmail.com>
2020-05-19 20:14:46 +0900

    06 - 03 - Order 클래스 추가

--- 06/03.js
+++ 06/03.js
@@ -6,3 +6,18 @@ function price(order) {
   const shipping = Math.min(basePrice * 0.1, 100);
   return basePrice - quantityDiscount + shipping;
 }
+
+class Order {
+  constructor(aRecord) {
+    this._data = aRecord;
+  }
+
+  get quantity() { return this._data.quantity; }
+  get itemPrice() { return this._data.itemPrice; }
+
+  get price() {
+    return order.quantity * order.itemPrice -
+      Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
+      Math.min(order.quantity * order.itemPrice * 0.1, 100);
+  }
+}
