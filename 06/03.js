89dfb2d4549483a3accba0f557f9c06acfe66e97
Jinho Hyeon <neverlish@gmail.com>
2020-05-19 20:15:58 +0900

    06 - 03 - Order 클래스 코드 개선

--- 06/03.js
+++ 06/03.js
@@ -16,8 +16,10 @@ class Order {
   get itemPrice() { return this._data.itemPrice; }
 
   get price() {
-    return order.quantity * order.itemPrice -
-      Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-      Math.min(order.quantity * order.itemPrice * 0.1, 100);
+    return this.basePrice - this.quantityDiscount + this.shipping;
   }
+
+  get basePrice() { return this.quantity * this.itemPrice; }
+  get quantityDiscount() { return Math.max(0, this.quantity - 500) * this.itemPrice * 0.05; }
+  get shipping() { return Math.min(this.basePrice * 0.1, 100); }
 }
