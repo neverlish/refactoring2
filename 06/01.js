f5e7e7c4ccd0509a19e57b3f17d4892641fd5396
Jinho Hyeon <neverlish@gmail.com>
2020-05-19 19:57:21 +0900

    06 - 01 - printDetails 함수 전역으로 이동

--- 06/01.js
+++ 06/01.js
@@ -14,17 +14,17 @@ function printOwing(invoice) {
   const today = Clock.today;
   invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
 
-  printDetails();
-
-  function printDetails() {
-    console.log(`고객명: ${invoice.customer}`);
-    console.log(`채무액: ${outstanding}`);
-    console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
-  }
+  printDetails(invoice, outstanding);
 }
 
 function printBanner() {
   console.log("***************");
   console.log("*** 고객 채무 ***");
   console.log("***************");
+}
+
+function printDetails(invoice, outstanding) {
+  console.log(`고객명: ${invoice.customer}`);
+  console.log(`채무액: ${outstanding}`);
+  console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
 }
