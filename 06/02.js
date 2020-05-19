9836d5aee7a278b7b6f0ee807cf34eed248357d5
Jinho Hyeon <neverlish@gmail.com>
2020-05-19 20:05:33 +0900

    06 - 02 - moreThanFiveLateDeliveries 인라인하기

--- 06/02.js
+++ 06/02.js
@@ -1,9 +1,5 @@
 // 06 - 02 함수 인라인하기
 
 function rating(aDriver) {
-  return moreThanFiveLateDeliveries(aDriver) ? 2 : 1;
-}
-
-function moreThanFiveLateDeliveries(aDriver) {
-  return aDriver.numberOfLateDeliveries > 5;
+  return aDriver.numberOfLateDeliveries > 5 ? 2 : 1;
 }
