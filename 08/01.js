6bb64bcfc67952f53cddaffaf15d58db84670084
Jinho Hyeon <neverlish@gmail.com>
2020-06-15 19:26:14 +0900

    08 - 01 - distance, radians 함수 최상위로 이동

--- 08/01.js
+++ 08/01.js
@@ -13,13 +13,13 @@ function trackSummary(points) {
   function calculateTime() { } // 총 시간 계산
 }
 
+function distance(p1, p2) { }
+function radians(degress) { }
+
 function totalDistance(points) {
   let result = 0;
   for (let i = 1; i < points.length; i++) {
     result += distance(points[i - 1], points[i]);
   }
   return result;
-
-  function distance(p1, p2) { }
-  function radians(degress) { }
 }
