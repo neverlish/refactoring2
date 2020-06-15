b9e80257bfae183d7c626a1d9e5629937a5f0ef2
Jinho Hyeon <neverlish@gmail.com>
2020-06-15 19:43:26 +0900

    08 - 01 - Account 클래스 추가

--- 08/01.js
+++ 08/01.js
@@ -23,3 +23,23 @@ function totalDistance(points) {
   }
   return result;
 }
+
+class Account {
+  get bankChange() {
+    let result = 4.5;
+    if (this._daysOverdrawn > 0) result += this.overdraftCharge;
+    return result;
+  }
+
+  get overdraftCharge() {
+    if (this.type.isPremium) {
+      const baseCharge = 10;
+      if (this.daysOverdrawn <= 7)
+        return baseCharge;
+      else
+        return baseCharge + (this.daysOverdrawn - 7) * 0.85;
+    } else {
+      return this.daysOverdrawn * 1.75;
+    }
+  }
+}
