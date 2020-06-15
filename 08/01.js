a7e46900dc76a6ee539455949ad1193319696ccc
Jinho Hyeon <neverlish@gmail.com>
2020-06-15 19:19:17 +0900

    08 - 01 - calculateDistance 함수 최상위로 복사

--- 08/01.js
+++ 08/01.js
@@ -23,3 +23,11 @@ function trackSummary(points) {
   function radians(degress) { } // 라디안 값으로 변환
   function calculateTime() { } // 총 시간 계산
 }
+
+function top_calculateDistance(pointsz) {
+  let result = 0;
+  for (let i = 1; i < points.length; i++) {
+    result += distance(points[i - 1], points[i]);
+  }
+  return result;
+}
