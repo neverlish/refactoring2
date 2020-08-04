a08a353623eab8075d2abb1ad0d423d3899bad7d
Jinho Hyeon <neverlish@gmail.com>
2020-08-04 21:06:52 +0900

    10 조건부 로직 간소화 - 01 조건문 분해하기 - 첫 코드

---
+++ 10/01.ts
@@ -0,0 +1,26 @@
+// 조건부 로직 간소화 - 1 조건문 분해하기
+
+interface IDate {
+  isBefore: (date: Date) => boolean;
+  isAfter: (date: Date) => boolean;
+}
+
+interface IPlan {
+  summerStart: Date;
+  summerEnd: Date;
+
+  summerRate: number;
+  regularRate: number;
+
+  regularServiceCharge: number;
+}
+
+function getCharge(aDate: IDate, plan: IPlan, quantity: number) {
+  let charge;
+  if (!aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd))
+    charge = quantity * plan.summerRate;
+  else
+    charge = quantity * plan.regularRate + plan.regularServiceCharge;
+
+  return charge;
+}
