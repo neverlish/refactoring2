46bb55ae4f28c6de02ef9a740cae4745a6bea7bc
Jinho Hyeon <neverlish@gmail.com>
2020-12-08 23:00:52 +0900

    12 - 06 - 서브클래스 추가

--- 12/6.ts
+++ 12/6.ts
@@ -22,9 +22,19 @@ class Engineer extends Employee {
   get type() { return 'engineer'; }
 }
 
+class Salesperson extends Employee {
+  get type() { return 'salesperson'; }
+}
+
+class Manager extends Employee {
+  get type() { return 'manager'; }
+}
+
 function createEmployee(name, type) {
   switch (type) {
     case 'engineer': return new Engineer(name, type);
+    case 'salesperson': return new Salesperson(name, type);
+    case 'manager': return new Manager(name, type);
   }
   return new Employee(name, type);
 }
