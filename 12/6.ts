0095a1171ce9ad3f4711302f8b2d06e9054499bf
Jinho Hyeon <neverlish@gmail.com>
2020-12-08 23:01:26 +0900

    12 - 06 - 슈퍼클래스의 게터 제거

--- 12/6.ts
+++ 12/6.ts
@@ -5,15 +5,12 @@ class Employee {
   constructor(name, type) {
     this.validateType(type)
     this._name = name;
-    this._type = type;
   }
 
   validateType(arg) {
     if (!['engineer', 'manager', 'salesperson'].includes(arg))
       throw new Error(`${arg}라는 직원 유형은 없습니다.`);
   }
-
-  get type() { return this._type; }
 
   toString() { return `${this._type} ${this.type}`; }
 }
