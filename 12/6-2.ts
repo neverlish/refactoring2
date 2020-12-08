8f7a4f4558c784ff52dc0a097a4e4e6b9239a7b0
Jinho Hyeon <neverlish@gmail.com>
2020-12-08 23:11:02 +0900

    12 - 06 - EmployeeType 구현

--- 12/6-2.ts
+++ 12/6-2.ts
@@ -13,14 +13,25 @@ class Employee {
       throw new Error(`${arg}라는 직원 유형은 없습니다.`);
   }
 
+  get typeString() { return this._type.toString(); }
   get type() { return this._type; }
   set type(arg) { this._type = arg; }
 
   get capitalizedType() {
-    return this._type.charAt(0).toUpperCase() + this._type.substr(1).toLowerCase();
+    return this.typeString.charAt(0).toUpperCase() + this.typeString.substr(1).toLowerCase();
   }
 
   toString() {
     return `${this._name} (${this.capitalizedType})`;
   }
+}
+
+class EmployeeType {
+  _value: string;
+
+  constructor(aString) {
+    this._value = aString;
+  }
+
+  toString() { return this._value; }
 }
